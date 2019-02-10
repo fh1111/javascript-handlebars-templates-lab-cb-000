a01203669bc4b7c7e7f0684e662392c90fdf7a5b
Fer Hernandez <fh.1111@gmail.com>
2019-02-10 19:28:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function init() {
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+
+function handlebarsSetup() {
+  //put any handlebars registrations here.
+  Handlebars.registerHelper('displayIngredient', function(ingredient) {
+    return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
+  })
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
+}
