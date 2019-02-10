bf1e6917d7fa1c56a7a71405107ebfcb543dad89
Fer Hernandez <fh.1111@gmail.com>
2019-02-10 19:28:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,9 @@ function handlebarsSetup() {
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
   Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
 }
+
+function initForm() {
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handlebars.compile(formTemplate)
+  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
+}
